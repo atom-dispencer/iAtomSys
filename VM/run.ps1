fc18ddf200c536feefec06797407898fdd468fe3
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2024-05-12 00:29:12 +0100

    Get CI/CD version into the Java build
    Hooray! I actually did it!
    Gradle (and GitHub Actions by extension) will now put the VM Version string passed to Gradle into a properties file which the running VM can read! This means that there is now a single source of truth for the version of the VM: the GitHub actions configuration.

--- VM/run.ps1
+++ VM/run.ps1
@@ -1,2 +1,10 @@
-./gradlew clean build --warning-mode all
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -jar .\build\libs\vm.jar
+# Build and test using a default version.
+# This is replaced with a live version in the GitHub actions CI/CD pipeline
+$VERSION = "DEV"
+echo "Building with version $VERSION"
+./gradlew build test -Pversion="$VERSION"
+
+# Start the jar with a debugger
+$JAR = ".\build\libs\vm-$VERSION.jar"
+echo "Starting $JAR"
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -jar "$JAR"
