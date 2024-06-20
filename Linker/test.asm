24b708c0625461c8a710c503c4547f3b9c1e7154
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2024-06-20 01:34:40 +0100

    Update test.asm

--- Linker/test.asm
+++ Linker/test.asm
@@ -5,30 +5,51 @@ extern WriteFile
 extern ExitProcess
 
 section .text
-  global main
-  main:
-      sub     rsp, 40          ; reserve shadow space and align stack by 16
+global  main
 
-      mov     rcx, -11         
-      call    GetStdHandle     ; Get STDOUT's handle
-      mov     rcx, rax         ; HANDLE is a 64-byte type on x64
+main:
+	;   RSP is stack pointer register!
+	;   Stack grows downwards in memory, so this is *expanding* the stack by 40 bytes.
+	;   reserve shadow space and align stack by 16
+	sub rsp, 40
 
-      lea     rdx, [msg]       ; lpBuffer = RIP-relative LEA (default rel)
-      mov     r8d, msg.len     ; DWORD nNumberOfBytesToWrite = 32-bit immediate constant length
-      lea     r9, [rsp+48]     ; lpNumberOfBytesWritten pointing into main's shadow space
-      mov     qword [rsp + 32], 0   ; lpOverlapped = NULL; This is a pointer, needs to be qword.
-      call    WriteFile        ; WriteFile(handle, msg, len, &our_shadow_space, NULL)
+	;    Get STDOUT's handle
+	;    -11 is the DWORD for STDOUT
+	;    HANDLE is a 64-byte type on x64
+	mov  rcx, -11
+	call GetStdHandle
 
-  ;;; BOOL return value in EAX (BOOL is a 4-byte type, unlike bool).
-  ;;; NumberOfBytesWritten in dword [rsp+48]
+	;    BOOL WriteFile(
+	;    [in]                 HANDLE       hFile
+	;    [in]                 LPCVOID      lpBuffer
+	;    [in]                 DWORD        nNumberOfBytesToWrite
+	;    [out, optional]      LPDWORD      lpNumberOfBytesWritten
+	;    [in, out, optional]  LPOVERLAPPED lpOverlapped
+	;    )
+	;    Remember that BOOL (not bool) is a 4-byte type!
+	;    -
+	;    Handle was returned in RAX, so move to RCX
+	;    lpBuffer = RIP-relative LEA (default rel)
+	;    DWORD nNumberOfBytesToWrite = 32-bit immediate constant length
+	;    &lpNumberOfBytesWritten: Address in main's shadow space to recieve the length written
+	;    lpOverlapped = NULL; This is a pointer, needs to be qword.
+	mov  rcx, rax
+	lea  rdx, [msg]
+	mov  r8d, msg.len
+	lea  r9, [rsp+48]
+	mov  qword [rsp + 32], 0
+	call WriteFile
 
-      xor     ecx, ecx
-      call ExitProcess
-    ; add     rsp, 40      ; alternative to calling a noreturn function like ExitProcess
-    ; ret
+	;   Zero out the ECX register, free the shadow space and exit
+	xor ecx, ecx
+	add rsp, 40
+	ret
 
+	; call ExitProcess
 
-section .data         ; or section .rdata to put it in a read-only page
-    msg:  db "-- iAtomSys Linker --", 21, 10    ; including a CR LF newline is a good idea for a line of output text
-    .len equ  $-msg    ; let the assembler calculate the string length
-                       ; .len is a local label that appends to the most recent non-dot label, so this is msg.len
+	section .data; or section .rdata to put it in a read-only page
+
+msg:
+	db   "-- iAtomSys Linker --", 21, 10; including a CR LF newline is a good idea for a line of output text
+	.len equ  $-msg; let the assembler calculate the string length
+	;    .len is a local label that appends to the most recent non-dot label, so this is msg.len
