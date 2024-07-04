2a47d6ed66caba421fe82eb1bc84482189ae7d27
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2024-07-04 11:19:45 +0100

    RunRequestPacket tests

--- VM/src/main/java/uk/iatom/iAtomSys/common/api/RunRequestPacket.java
+++ VM/src/main/java/uk/iatom/iAtomSys/common/api/RunRequestPacket.java
@@ -2,20 +2,26 @@ package uk.iatom.iAtomSys.common.api;
 
 public record RunRequestPacket(String startAddress) {
 
+  public static final int MAX_LENGTH = 10;
+  public static final String ERR_ADDRESS_NULL = "Start address may not be null";
+  public static final String ERR_ADDRESS_BLANK = "Start address may not be blank";
+
+  public static String ERR_ADDRESS_LONG(String startAddress) {
+    return"Start address is too long: %d>%d".formatted(startAddress.length(), MAX_LENGTH);
+  }
+
   public RunRequestPacket {
 
     if (startAddress == null) {
-      throw new IllegalStateException("Start address may not be null");
+      throw new IllegalArgumentException(ERR_ADDRESS_NULL);
     }
 
-    int MAX_LENGTH = 10;
     if (startAddress.length() > MAX_LENGTH) {
-      throw new IllegalStateException(
-          "Start address is too long: %d>%d".formatted(startAddress.length(), MAX_LENGTH));
+      throw new IllegalArgumentException(ERR_ADDRESS_LONG(startAddress));
     }
 
     if (startAddress.isBlank()) {
-      throw new IllegalStateException("Start address may not be blank");
+      throw new IllegalArgumentException(ERR_ADDRESS_BLANK);
     }
   }
 }
