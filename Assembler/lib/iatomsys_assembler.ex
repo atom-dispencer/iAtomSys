8b2754b0e8d5f514c63a5ab5d1edcd5fbc049a24
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2024-08-23 16:10:06 +0100

    Run script

--- Assembler/lib/iatomsys_assembler.ex
+++ Assembler/lib/iatomsys_assembler.ex
@@ -1,6 +1,8 @@
 defmodule IAtomSysAssembler.CLI do
   def main(args) do
-    IO.puts("iAtomSys Assembler")
+    IO.puts("")
+    IO.puts(" ~~ iAtomSys Assembler (Elixir) ~~")
+    IO.puts("")
     {opts, _, _} = OptionParser.parse(args, switches: [file: :string], aliases: [f: :file])
     IO.inspect(opts)
 
