4c142a8a9b280b38b1a65789247764eea171c7f3
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2024-08-29 16:54:58 +0100

    Mutually exclusive command line argument parsing

--- Assembler/app/Main.hs
+++ Assembler/app/Main.hs
@@ -2,28 +2,60 @@ module Main where
 
 import Options.Applicative
 
+data Mode = ASSEMBLE_ONLY | LINK_ONLY | BOTH deriving (Eq)
+
+instance Show Mode where
+  show ASSEMBLE_ONLY = "assemble_only"
+  show LINK_ONLY = "link_only"
+  show BOTH = "assemble_and_link"
+
+data Verbosity = SILENT | NORMAL | VERBOSE deriving (Eq)
+
+instance Show Verbosity where
+  show SILENT = "silent"
+  show NORMAL = "normal"
+  show VERBOSE = "verbose"
+
 data Options = Options
   { file :: String,
-    compile :: Bool,
-    link :: Bool,
-    silent :: Bool,
-    verbose :: Bool
+    mode :: Mode,
+    verbosity :: Verbosity
   }
+
+{- The Options.Applicative Parser for the application's command-line arguments -}
+verbosityVerbose :: Options -> Bool
+verbosityVerbose = (VERBOSE ==) . verbosity
+
+verbositySilent :: Options -> Bool
+verbositySilent = (NORMAL ==) . verbosity
+
+modeAssemble :: Options -> Bool
+modeAssemble = (ASSEMBLE_ONLY ==) . mode
+
+modeLink :: Options -> Bool
+modeLink = (LINK_ONLY ==) . mode
 
 optionParser :: Parser Options
 optionParser =
   Options
-    <$> strOption (long "file" <> short 'f' <> help "The input file or directory for the program")
-    <*> switch (long "compile" <> short 'c' <> help "Compile the given file(s)")
-    <*> switch (long "link" <> short 'l' <> help "Link the given file(s)")
-    <*> switch (long "silent" <> short 's' <> help "Suppress all output messages")
-    <*> switch (long "verbose" <> short 'v' <> help "Produce *additional* logging messages")
+    <$> strOption
+      ( long "file"
+          <> short 'f'
+          <> help "The input file or directory for the program."
+      )
+    <*> ( flag' ASSEMBLE_ONLY (short 'a' <> long "assemble")
+            <|> flag' LINK_ONLY (short 'l' <> long "link")
+            <|> pure BOTH
+        )
+    <*> ( flag' SILENT (short 's' <> long "silent")
+            <|> flag' VERBOSE (short 'v' <> long "verbose")
+            <|> pure NORMAL
+        )
 
 {- The main function for the assembler and the application's entrypoint. -}
 main :: IO ()
 main =
-  credits >> parseProgramOptions >>= \opts ->
-    displayArgs opts >> start opts
+  credits >> parseProgramOptions >>= \opts -> displayArgs opts >> start opts
 
 {- Display credits, links and licensing information for the application. -}
 credits :: IO ()
@@ -46,15 +78,16 @@ parseProgramOptions = execParser opts
             <> header "Hiya! We are testing iasm!"
         )
 
-{- Display the programs arguments. -}
+{- Display the programs arguments (or not, if we're silent ;) ) -}
 displayArgs :: Options -> IO ()
-displayArgs (Options f c l s v) = do
-  putStrLn $ "file: " ++ f
-  putStrLn $ "compile: " ++ show c
-  putStrLn $ "link: " ++ show l
-  putStrLn $ "silent: " ++ show s
-  putStrLn $ "verbose: " ++ show v
+displayArgs (Options _ _ SILENT) = return ()
+displayArgs (Options f m v) = do
+  putStrLn "Parsed arguments: "
+  putStrLn $ "file      : " ++ f
+  putStrLn $ "mode      : " ++ show m
+  putStrLn $ "verbosity : " ++ show v
+  putStrLn ""
 
 {- Start assembling! -}
 start :: Options -> IO ()
-start (Options a b c d e) = putStrLn "Doing things..."
+start (Options _ _ _) = putStrLn "Doing things..."
