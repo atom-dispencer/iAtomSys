20280105d55884a7b4f0ad3be851d6be7cae904e
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2024-09-04 15:20:22 +0100

    Docs for assembly steps.

--- Assembler/app/IASM/Assembler.hs
+++ Assembler/app/IASM/Assembler.hs
@@ -1,7 +1,10 @@
 module IASM.Assembler where
 
-import Data.Array.Byte (ByteArray)
 import Data.List (unfoldr)
+
+type Token = String
+
+type Statement = String
 
 assembleFiles :: [FilePath] -> Int -> IO ()
 assembleFiles files batchCount = do
@@ -20,11 +23,31 @@ splitListEvenly chunkSize = unfoldr split
     split xs = Just (splitAt chunkSize xs)
 
 assembleSingle :: FilePath -> IO ()
-assembleSingle = getLines . tokenise . parse . preprocess . compile . writeToObject
+assembleSingle path = do
+  content <- readFile path
+  let bytes = compile . preprocess . parse . tokenise . lines $ content
+  writeToObject bytes
 
-getLines :: FilePath -> [String]
+{- Convert a list of lines to a list of Tokens representing discrete atoms of information
+ - within the compilation target. -}
 tokenise :: [String] -> [Token]
+tokenise fileLines = fileLines
+
+{- Group the given list of Tokens into coherent Statements, for example 'MOV IDK TBH*' -}
 parse :: [Token] -> [Statement]
+parse tokens = tokens
+
+{- Perform preprocessing steps on the given list of statements, such as applying macros.
+ - Macros are a text-replacement mechanism (like in C/C++) and are therefore not shared
+ - between source files, and hence can be consumed without linking. -}
 preprocess :: [Statement] -> [Statement]
-compile :: [Statement] -> ByteArray
-writeToObject :: ByteArray -> IO ()
+preprocess statements = statements
+
+{- Convert the given statements into bytes (or rather int16's). The informatio to do this
+ - may be incomplete, so space may be reserved for linking. -}
+compile :: [Statement] -> String
+compile statements = "nothing here yet..."
+
+{- Write a compiled (but not linked) iASM object (.oasm) back to the disk. -}
+writeToObject :: String -> IO ()
+writeToObject bytes = writeFile "output.txt" bytes
